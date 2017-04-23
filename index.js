a6e7969ac3c2808e5d635ef7f7d6e9eda07d4efb
Or Adar <adaror@gmail.com>
2017-04-23 08:35:23 +0300

    Fixing the event Emmiter!

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 var player = require("./player");
 var eventConfig = require("./config").events;
 
-var player1 = new player('Or')
-console.log(player1);
+var player1 = new player('Arik');
+var player2 = new player('Or');
 player1.addMedals(5);
-player1.on(eventConfig.medalChange,function(){
-    console.log(`hey!`);
-});
+player1.addBaskets(50);
+player2.addBaskets(15);
+player2.addBaskets(20);
+
--- player.js
+++ player.js
@@ -3,29 +3,36 @@ var eventConfig = require("./config").events;
 var Emmiter = require('events');
 var myEmtr = new Emmiter();
 
-
 class Player{
-
     constructor(name) {
         this.name = name;
         this.medals = 0;
         this.baskets = 0;
-    }
+    };
 
     addMedals(medals) {
         this.medals += medals;
-        this.emit(eventConfig.medalChange);
-    }
+        myEmtr.emit(eventConfig.medalChange,this.name,this.medals);
+    };
 
     addBaskets(bask){
         this.baskets += bask;
-        this.emit(eventConfig.basketsChange);
-    }
+        myEmtr.emit(eventConfig.basketsChange,this.name,this.baskets);
+    };
 
 
-}
+
+};
+
 util.inherits(Player,Emmiter.EventEmitter);
 
+myEmtr.on(eventConfig.medalChange,function(name,medals){
+    console.log(`${name} have ${medals} medals!!`);
+});
+
+myEmtr.on(eventConfig.basketsChange,function(name,baskets){
+    console.log(`${name} scored ${baskets} basktets!`);
+})
 
 module.exports = Player;
 
